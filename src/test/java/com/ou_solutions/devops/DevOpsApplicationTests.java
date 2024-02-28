ca52ee3e18210c2d0d9345bef1105235779f9bfa
Omkar Upadhyay <omkarmh009@gmail.com>
2024-02-28 22:12:14 +0800

    Initial DevOps Commit

---
+++ src/test/java/com/ou_solutions/devops/DevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ou_solutions.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
